35663951169c04efc94f71efc91757ca2e5e178c
hz <dxp@ngs.ru>
2015-09-29 17:29:55 +0600

    add new options: change copyright year and author name

--- src/titlemod.py
+++ src/titlemod.py
@@ -52,12 +52,18 @@ class Err(Exception):
 #
 def process_options(options):
 
-    optlist, infiles = getopt.gnu_getopt(options, 'rd:v:')
+    optlist, infiles = getopt.gnu_getopt(options, 'rd:v:n:y', ['remove-svn-keywords'])
+
+    print  optlist
 
     options = {}
     options['Recursive Dirs'] = False
     options['Dirs'] = []
     options['Version'] = None
+    options['Name'] = None
+    options['Touch Year'] = False
+    options['Remove SVN Keywords'] = False
+
 
     for i in optlist:
         if i[0] == '-r':
@@ -70,6 +76,15 @@ def process_options(options):
 
         if i[0] == '-v':
             options['Version'] = i[1]
+
+        if i[0] == '-n':
+            options['Name'] = i[1]
+
+        if i[0] == '-y':
+            options['Touch Year'] = True
+
+        if i[0] == '--remove-svn-keywords':
+            options['Remove SVN Keywords'] = True
 
    # if not options.has_key('Version'):
    #     raise Err('Error: ')
@@ -107,7 +122,7 @@ def get_file_list( d, ext ):
 #
 #    Modifies titles in the file: version number and  copyright year
 #
-def modify_title(f, vernum, year):
+def modify_title(f, vernum, year, name, rm_svn):
 
     in_file = open(f, 'rb').read()
     if in_file.find('\r\n') != -1:
@@ -129,18 +144,23 @@ def modify_title(f, vernum, year):
                 oldver = res.group(0)
                 in_file[idx] = i.replace(oldver, 'Version: ' + vernum, 1)
 
-        res = re.search('Copyright( |\t)+\(c\)( |\t)+([0-9- ]+)', i)
+        res      = re.search('Copyright[ \t]+\(c\)[ \t]+([0-9- ]+)\,[ \t]+(.+)', i)
         if res:
-            Copyright = res.group(0)
-            yres = re.search('([0-9]+).*', Copyright)
-            if yres:
+            years    = res.groups()[0]
+            old_name = res.groups()[1]
+            new_str  = ''
+
+            yres = re.search('([0-9]+).*', years)
+            if year and yres:
                 y = yres.group(1)
                 if y != year:
                     newdate = y + '-' + year
-                    in_file[idx] = i.replace(yres.group(0), newdate, 1)
+                    new_str = i.replace(yres.group(0), newdate, 1)
 
+            if name:
+                new_str = new_str.replace(old_name, name, 1)
 
-
+            in_file[idx] = new_str
 
     out_file = ''
     for i in in_file:
@@ -155,16 +175,24 @@ def modify_title(f, vernum, year):
 #    Gets arguments:
 #        infiles - input files list (files to modify)
 #        options - dictionary with the following options:
-#         +-------------------------------------------------------+
-#         |  Key             |  Value                             |
-#         +------------------+------------------------------------+
-#         | 'Dirs'           | List of directories to process     |
-#         |                  |                                    |
-#         | 'Recursive Dirs' | Boolean flag, specifies to process |
-#         |                  | input directiories recursively     |
-#         |                  |                                    |
-#         | 'Version'        | String with the new version name   |
-#         +-------------------------------------------------------+
+#         +---------------------- + -----------------------------------+
+#         |  Key                  |  Value                             |
+#         +---------------------- +------------------------------------+
+#         | 'Dirs'                | List of directories to process     |
+#         |                       |                                    |
+#         | 'Recursive Dirs'      | Boolean flag specifies to process  |
+#         |                       | input directiories recursively     |
+#         |                       |                                    |
+#         | 'Version'             | String with the new version name   |
+#         |                       |                                    |
+#         | 'Name'                | String with the new copyright name |
+#         |                       |                                    |
+#         | 'Year'                | Boolean flag specifies to touch    |
+#         |                       | copyright year                     |
+#         |                       |                                    |
+#         | 'Remove SVN keywords' | Boolean flag specifies to remove   |
+#         |                       | $Revision $ and $Date $ fields     |
+#         +-----------------------+------------------------------------+
 #
 def tmod(infiles, options):
 
@@ -201,10 +229,16 @@ def tmod(infiles, options):
     #
     #for i in files: print i
 
-    year = time.strftime('%Y')
+    if options['Touch Year']:
+        year = time.strftime('%Y')
+    else:
+        year = None
+
     vernum = options['Version']
+    name   = options['Name']
+    rm_svn = options['Remove SVN Keywords']
     for i in files:
-        modify_title(i, vernum, year)
+        modify_title(i, vernum, year, name, rm_svn)
 
 #-------------------------------------------------------------------------------
 #
@@ -223,9 +257,13 @@ def main():
         print '    usage: titlemod.py <files> <options>'
         print '       where <files> - one or more filenames of files to process'
         print '             <options>:'        
-        print '                  -d <dir>    - specify directory to process (all files in dir) '
-        print '                  -r          - process directories recursively'
-        print '                  -v <VerNum> - specify new version number'
+        print '                  -d <dir>    : specify directory to process (all files in dir) '
+        print '                  -r          : process directories recursively'
+        print '                  -v <VerNum> : specify new version number'
+        print '                  -n <Name    : specify new copyright name'
+        print '                  -y          : touch copyright year'
+        print ''
+        print '                  --remove-svn-keywords: remove $Rev $ and $Date $ fields'
 
 
     tmod(infiles, options)
