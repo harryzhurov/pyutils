eff10a44e5dcc7a7b7d605edbf9251bcd8b5ba7d
hz <dxp@ngs.ru>
2015-09-29 13:52:09 +0600

    add title modifier utility

---
+++ src/titlemod.py
@@ -0,0 +1,237 @@
+#*******************************************************************************
+#
+#             Source file title modifier
+#
+#                    Version 1.3
+#
+#          Copyright (c) 2006-2010, Harry E. Zhurov
+#
+#
+#
+#    DESCRIPTION:  The utility performs modification of source file headers.
+#                  Header must be at top of file and consists of commented 
+#                  lines (commented with line comments - //). Header must not
+#                  contain blank or not commented lines - any such line means 
+#                  the end of the header and the utility stops processing in 
+#                  this case.
+# 
+#                  Modifications are: copyright date and version number.
+#
+#                  Copyright must be in form: 'Copyright (c) <ybegin>-<yend>, <author>',
+#                  where <ybegin> - copyright begin year, <yend> - end year; <yend> is
+#                  optional.
+#
+#                  Version must be in form: 'Version: <version>', where <version> can be
+#                  any text.
+#
+#*******************************************************************************
+
+import  sys
+import  string
+import  getopt
+import  os
+import  time
+import  re
+import  glob
+
+
+Extensions = ['c', 'cpp', 'h', 'hpp', 
+              'asm', 's90', 's43', 's79', 'sbf', 's', 'S']
+
+
+#-------------------------------------------------------------------------------
+#
+#    Exception class
+#
+class Err(Exception):
+    pass
+
+#-------------------------------------------------------------------------------
+#
+#    Process options
+#
+def process_options(options):
+
+    optlist, infiles = getopt.gnu_getopt(options, 'rd:v:')
+
+    options = {}
+    options['Recursive Dirs'] = False
+    options['Dirs'] = []
+    options['Version'] = None
+
+    for i in optlist:
+        if i[0] == '-r':
+            options['Recursive Dirs'] = True
+
+        if i[0] == '-d':
+            d = options['Dirs']
+            d.append(i[1])
+            #options['Files'] = d
+
+        if i[0] == '-v':
+            options['Version'] = i[1]
+
+   # if not options.has_key('Version'):
+   #     raise Err('Error: ')
+
+    return infiles, options
+#-------------------------------------------------------------------------------
+#
+#    Prepare directories list from specified dir name
+#
+def get_dir_list(d, recursive = True):
+    dlist = os.listdir(d)
+    dirs = []
+    for i in dlist:
+        ditem = d + '\\' + i
+        if os.path.isdir(ditem):
+            dirs.append(ditem)
+            if recursive:
+                dl = get_dir_list(ditem)
+                for j in dl:
+                    dirs.append(j)
+
+    return dirs
+#-------------------------------------------------------------------------------
+#
+#    Prepare file list based on specified extensions
+#
+def get_file_list( d, ext ):
+    files = []
+    for i in ext:
+        fl = glob.glob(d + '\\*.' + i)
+        files += fl
+
+    return files
+#-------------------------------------------------------------------------------
+#
+#    Modifies titles in the file: version number and  copyright year
+#
+def modify_title(f, vernum, year):
+
+    in_file = open(f, 'rb').read()
+    if in_file.find('\r\n') != -1:
+        sep = '\r\n'
+    else:
+        sep = '\n'
+
+    in_file = in_file.split(sep)
+
+    for i in in_file:
+        res = re.search('^(\/|\*|\ \*|;).*( |\t)*', i)
+        if not res: 
+            break
+
+        idx = in_file.index(i)
+        if vernum:
+            res = re.search('Version:(\s+)([a-zA-Z0-9-\.]+)', i)
+            if res:
+                oldver = res.group(0)
+                in_file[idx] = i.replace(oldver, 'Version: ' + vernum, 1)
+
+        res = re.search('Copyright( |\t)+\(c\)( |\t)+([0-9- ]+)', i)
+        if res:
+            Copyright = res.group(0)
+            yres = re.search('([0-9]+).*', Copyright)
+            if yres:
+                y = yres.group(1)
+                if y != year:
+                    newdate = y + '-' + year
+                    in_file[idx] = i.replace(yres.group(0), newdate, 1)
+
+
+
+
+    out_file = ''
+    for i in in_file:
+        out_file += i + sep
+
+    open(f, 'wb').write(out_file[:-len(sep)])
+
+#-------------------------------------------------------------------------------
+#
+#    Title modify function: carry out main functionality
+#
+#    Gets arguments:
+#        infiles - input files list (files to modify)
+#        options - dictionary with the following options:
+#         +-------------------------------------------------------+
+#         |  Key             |  Value                             |
+#         +------------------+------------------------------------+
+#         | 'Dirs'           | List of directories to process     |
+#         |                  |                                    |
+#         | 'Recursive Dirs' | Boolean flag, specifies to process |
+#         |                  | input directiories recursively     |
+#         |                  |                                    |
+#         | 'Version'        | String with the new version name   |
+#         +-------------------------------------------------------+
+#
+def tmod(infiles, options):
+
+    curdir = os.getcwd()
+
+    #-------------------------------------------------------------
+    #
+    #   Prepare file list
+    #
+    dirlist = []
+    if infiles:
+        files = infiles
+
+    else:
+        if(options['Dirs']):
+            dl = options['Dirs']
+            for i in dl:
+                dirlist.append(i)
+                dirlist += get_dir_list(i, options['Recursive Dirs'])
+
+        else:
+            dirlist = get_dir_list(curdir, options['Recursive Dirs'])
+            dirlist.insert(0, curdir)
+
+        files = []
+        for i in dirlist:
+            f = get_file_list(i, Extensions)
+            for j in f:
+                files.append(j)
+
+    #------------------------------------------------------------
+    #
+    #   Get current year
+    #
+    #for i in files: print i
+
+    year = time.strftime('%Y')
+    vernum = options['Version']
+    for i in files:
+        modify_title(i, vernum, year)
+
+#-------------------------------------------------------------------------------
+#
+#    Main function: get options and arguments, run tasks
+#
+def main():
+    try:
+        infiles, options = process_options(sys.argv[1:])
+
+    except Exception, x:
+        print 'Error:', x
+        sys.exit(1)
+
+    if not infiles and not options['Dirs']:
+        print 'Title modifier. Modifies copyright date and/or version number.'
+        print '    usage: titlemod.py <files> <options>'
+        print '       where <files> - one or more filenames of files to process'
+        print '             <options>:'        
+        print '                  -d <dir>    - specify directory to process (all files in dir) '
+        print '                  -r          - process directories recursively'
+        print '                  -v <VerNum> - specify new version number'
+
+
+    tmod(infiles, options)
+
+#-------------------------------------------------------------------------------
+if __name__ == '__main__': 
+    main()
+#-------------------------------------------------------------------------------
+
