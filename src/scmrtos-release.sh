97fe32093df3087f9cf6e9653b46d704b8e73039
hz <dxp@ngs.ru>
2015-11-25 21:45:57 +0600

    add line endings replacement to CRLF for 'scmrtos-release.sh'

--- src/scmrtos-release.sh
+++ src/scmrtos-release.sh
@@ -3,16 +3,24 @@
 RTOS_NAME=scmrtos
 SAMPLES_NAME=scmrtos-sample-projects
 
-RTOS_URL=https://github.com/harryzhurov/$RTOS_NAME.git
-SAMPLES_URL=https://github.com/scmrtos/$SAMPLES_NAME.git
+RTOS_URL=https://github.com/$RTOS_NAME/$RTOS_NAME.git
+SAMPLES_URL=https://github.com/$RTOS_NAME/$SAMPLES_NAME.git
 
 DOC_PATH=~/pro/scmRTOS/scmrtos-doc
 
+#--------------------------------------------------
+#
+#    Check argument[s]
+#
 if [ -z $1 ]; then
     echo 'E: release number must be specified'
 	exit
 fi
 
+#--------------------------------------------------
+#
+#    Clean up working directory
+#
 RELEASE_NAME=$RTOS_NAME'-release-'$1
 
 if [ -e $RELEASE_NAME ]; then
@@ -26,18 +34,53 @@ if [ -e $RELEASE_NAME ]; then
     fi
 fi
 
-git clone --recursive -b develop $SAMPLES_URL $RELEASE_NAME
+#--------------------------------------------------
+#
+#    Get data  
+#
+git clone $SAMPLES_URL $RELEASE_NAME
 
 if [ $? -ne 0 ]; then
     echo 'E: clone operation unsuccessfull'
     exit
 fi
 
+#--------------------------------------------------
+#
+#    Replace line endings to windows style 
+#
+cd $RELEASE_NAME
+ls | xargs rm -rf
+echo '* text eol=crlf' > .gitattributes
 
+#read -n1 -r -p "Press any key to continue..." key
+
+git reset --hard
+git submodule init
+git submodule update
+
+#--------------------------------------------------
+#
+#    Replace line endings to windows style (in submodule)
+#
+cd scmRTOS
+ls | xargs rm -rf
+echo '* text eol=crlf' > .gitattributes
+git reset --hard
+cd ..
+
+#--------------------------------------------------
+#
+#    Remove service data
+#
 cd $RELEASE_NAME
 rm -rf .git*
 rm -rf scmRTOS/.git*
 
+#--------------------------------------------------
+#
+#    Put documentation
+#
 mkdir -p doc/
 
 cp -R $DOC_PATH/pdf/* doc
@@ -48,6 +91,10 @@ fi
 
 cd ..
 
+#--------------------------------------------------
+#
+#    Create archive
+#
 if [ -f $RELEASE_NAME.zip ]; then
     rm $RELEASE_NAME.zip
 
