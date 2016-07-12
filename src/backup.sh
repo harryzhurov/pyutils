62cc5fff4bc9a5d703589ff37a41548f61143c89
hz <dxp@ngs.ru>
2016-07-12 13:38:55 +0600

    improve backup script

--- src/backup.sh
+++ src/backup.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-rsync -rvz ~/pro root@5.128.60.74:/nas/vol2/backup
-rsync -rvz ~/slon root@5.128.60.74:/nas/vol2/backup
-rsync -rvz /pool/cad/kicad/tools root@5.128.60.74:/nas/vol2/backup
+RSYNC=$(which rsync)
+
+$RSYNC -rvz ~/pro root@5.128.60.74:/nas/vol2/backup
+$RSYNC -rvz ~/slon root@5.128.60.74:/nas/vol2/backup
+$RSYNC -rvz /pool/cad/kicad/tools root@5.128.60.74:/nas/vol2/backup
+
+date --rfc-3339=seconds >> ~/backup.log
