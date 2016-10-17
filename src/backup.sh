f2185493219eda1f87baf1281ea59d8d4fc232ee
hz <dxp@ngs.ru>
2016-10-17 18:58:09 +0700

    add password base file backup

--- src/backup.sh
+++ src/backup.sh
@@ -4,6 +4,7 @@ RSYNC=$(which rsync)
 
 $RSYNC -rvz ~/pro root@5.128.60.74:/nas/vol2/backup
 $RSYNC -rvz ~/slon root@5.128.60.74:/nas/vol2/backup
-$RSYNC -rvz /pool/cad/kicad/tools root@5.128.60.74:/nas/vol2/backup
+$RSYNC -rvz /opt/cad/kicad/tools root@5.128.60.74:/nas/vol2/backup
+$RSYNC -rvz ~/pass.kdbx root@5.128.60.74:/nas/vol2/backup
 
 date --rfc-3339=seconds >> ~/backup.log
