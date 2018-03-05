70a12a8a23878a5bdb0f708f42ac59796a20174e
hz <dxp@ngs.ru>
2018-03-05 14:59:24 +0700

    modify codec script

--- src/video-cnv.sh
+++ src/video-cnv.sh
@@ -5,4 +5,9 @@ name=$(basename --suffix=mp4 $1)
 oname="${name}avi"
 echo "output file: "$oname
 
-ffmpeg -i $1 -vcodec copy -acodec mp3 -ab 128k $oname
+# default $2 = 320k
+# $3 - additional options, for example, resolution: -s 640x356
+
+ffmpeg -i $1 -c:v libxvid $3 -b:v $2 -vtag DX50 -c:a libmp3lame -b:a 128k $oname
+
+
